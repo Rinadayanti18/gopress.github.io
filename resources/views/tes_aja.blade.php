665432a6048627f9d139e31d2a16f3ff25fd08a9
kevinapul <akunapul11@gmail.com>
2022-08-20 10:44:33 +0700

    gg_gemink

--- resources/views/tes_aja.blade.php
+++ resources/views/tes_aja.blade.php
@@ -1,6 +1,14 @@
 ganteng broookkkk
 tesss tesss maniss
-mantap broooo
+<<<<<<< HEAD PALA
 
-<<<<<<< HEAD
+aku mau tes lagi yo
+
+ganti aja
+
 brisiiikkkkk
+>>>>>>> e5d466e082eeb63ed13d02c6752cd95cb9945fbf
+
+ada apa ni
+haloo againn
+apa aja yang updarte
